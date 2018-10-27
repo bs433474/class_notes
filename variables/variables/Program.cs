d70b44b76b986142386f21ffa3edbef1dfa142b7
Code 4 <code4@admin4s-MacBook.local>
2018-10-27 12:23:07 -0400

    first commit

---
+++ variables/variables/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string toy = "Action Figure";  
+
+            bool playPiano = true;
+
+            bool trigger = false;
+
+            decimal number = 41.9m;
+
+            int numberofPeople = 100;
+
+            char letter = 'W';
+
+
+
+        }          
+    }
+}
+
+
